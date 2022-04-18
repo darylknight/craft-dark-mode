ac777964404edaf56654496d22229a50740e8203
Daryl Knight <daryl@theengineisred.com>
2022-04-18 11:25:26 +0300

    Chunked out the SCSS file, added plugins store & redactor updates

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,8 +1,15 @@
 const mix = require('laravel-mix');
+const primarySiteUrl = process.env.PRIMARY_SITE_URL; // Get value from .env to use as Browsersync proxy URL
 
-mix.setPublicPath('./dist')
+mix.setPublicPath('./public/dist')
 
 	.sass('src/cp.scss', 'dist/css')
+
+	.browserSync({
+		files: ['public/assets/css/*', 'public/assets/js/*', 'templates/**/*'], // BrowserSync will refresh every time one of these files changes
+		proxy: primarySiteUrl, // This takes PRIMARY_SITE_URL from the .env file so devs can have different local URLs
+		notify: false,
+	})
 
 	.version()
 	.disableNotifications();
