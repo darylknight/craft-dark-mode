b9a9a0a1d849616fa0dcab12a9f3e03bf6a18107
Daryl Knight <daryl@theengineisred.com>
2022-04-21 23:09:42 +0300

    Fixed link to hosted CSS file

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,12 +1,12 @@
 const mix = require('laravel-mix');
 const primarySiteUrl = process.env.PRIMARY_SITE_URL; // Get value from .env to use as Browsersync proxy URL
 
-mix.setPublicPath('./public/dist')
+mix.setPublicPath('./dist')
 
 	.sass('src/cp.scss', 'dist/css')
 
 	.browserSync({
-		files: ['public/assets/css/*', 'public/assets/js/*', 'templates/**/*'], // BrowserSync will refresh every time one of these files changes
+		files: ['assets/css/*', 'assets/js/*', 'templates/**/*'], // BrowserSync will refresh every time one of these files changes
 		proxy: primarySiteUrl, // This takes PRIMARY_SITE_URL from the .env file so devs can have different local URLs
 		notify: false,
 	})
